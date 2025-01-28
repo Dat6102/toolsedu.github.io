710c8a41f7ac2704b03a37ca48dcb3c191d0f00d
Kiyoshi (Datz) <97487745+Dat6102@users.noreply.github.com>
2025-01-28 16:36:36 +0700

    Delete js directory

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-function goToCanbangpt() {
-    window.location.href = "canbangpt.html";
-}
-function goToAi() {
-    window.location.href = "ai.html";
-}
-function goToTudientienganh() {
-    window.location.href = "tudientienganh.html";
-}
